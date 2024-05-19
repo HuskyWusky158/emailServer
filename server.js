40fbb05c30e0577a28ff14728317eca1007c2e67
HuskyWusky158 <baxc@oregonstate.edu>
2024-05-19 15:23:13 -0700

    Add email server file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.post('/emailServer', (req, res) => {
+    const emailData = req.body;
+
+    // Send a response back to the client
+    res.send('Email received successfully!', emailData);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
