7fa28db2bda33bfdc18d7ef445f1823290572bba
HuskyWusky158 <baxc@oregonstate.edu>
2024-05-19 15:51:11 -0700

    Add node mailer

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import nodemailer from 'nodemailer';
 const express = require('express')
 const app = express()
 const port = 3000
@@ -11,8 +12,26 @@ app.get("/", (req, res) => {
 app.post('/emailServer', (req, res) => {
     const emailData = req.body;
 
-    //Add logic here
-
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        port: 465,
+        secure: true, 
+        auth: {
+            user: 'easyconversions158@gmail.com',
+            pass: 'uiio wcws lhig bllx'
+        }
+    });
+    
+    transporter.sendMail({
+        to: 'easyconversions158@gmail.com',
+        subject: 'hello',
+        html: '<h1>Hi how are you</h1>'
+    }).then(()=>{
+        console.log('Email sent');
+    
+    }).catch(err=>{
+        console.error(err);
+    });
 
     res.json({ message: 'Email sent successfully!', emailData });
 })
